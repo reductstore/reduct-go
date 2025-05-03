5c5430fef0d33844008c3598b160968050f1bffd
Shambel Amare <shambelamare2006@gmail.com>
2025-05-03 21:28:55 +0300

    add version file

--- tests/health_test.go
+++ tests/health_test.go
@@ -3,31 +3,26 @@ package tests
 import (
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReductStoreHealth(t *testing.T) {
 	healthUrl := "http://127.0.0.1:8383/api/v1/alive"
 
 	req, err := http.NewRequest(http.MethodHead, healthUrl, nil)
-	if err != nil {
-		t.Fatalf("Failed to create request: %v", err)
-	}
+	assert.NoError(t, err)
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatalf("Failed to send request: %v", err)
-	}
+	assert.NoError(t, err)
+
 	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			t.Fatalf("Failed to close response body: %v", err)
-		}
+		assert.NoError(t, resp.Body.Close())
 	}()
 
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
-	}
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
 
 }
