ba3879c0661f4a6622f82eb6579b2fd08896ba7f
Shambel Amare <shambelamare2006@gmail.com>
2025-05-12 04:41:36 +0300

    add entry writter and reader test

--- tests/bucket_test.go
+++ tests/bucket_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
 	"reduct-go/model"
@@ -41,4 +42,37 @@ func TestRemoveBucket(t *testing.T) {
 	// check if the bucket exists
 	err := mainTestBucket.Remove(context.Background())
 	assert.NoError(t, err)
+}
+
+func TestEntryRecordWritterAndReader(t *testing.T) {
+	exists, err := mainTestBucket.CheckExists(context.Background())
+	assert.NoError(t, err)
+	assert.True(t, exists)
+
+	// create a new entry record writter
+	writter := mainTestBucket.BeginWrite("entry-1", nil)
+	data := map[string]any{
+		"key1": "value1",
+		"key2": float64(2),
+		"key3": map[string]any{
+			"key3.1": "value3.1",
+			"key3.2": 3.2,
+		},
+	}
+	dataByte, err := json.Marshal(data)
+	assert.NoError(t, err)
+	err = writter.Write(dataByte, int64(len(dataByte)))
+	assert.NoError(t, err)
+	// we should be able to read the written data
+	reader, err := mainTestBucket.BeginRead(context.Background(), "entry-1", nil, nil, false)
+	assert.NoError(t, err)
+	// read the data
+	readData, err := reader.Read()
+	assert.NoError(t, err)
+	// check if the data is the same
+	var readDataMap map[string]any
+	err = json.Unmarshal(readData, &readDataMap)
+	assert.NoError(t, err)
+	// check if the data is the same
+	assert.Equal(t, data, readDataMap)
 }
