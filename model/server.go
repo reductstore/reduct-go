0e3d3f036f5542375ef97573c060587563f209c4
Shambel Amare <shambelamare2006@gmail.com>
2025-05-29 02:38:26 +0300

    remove unsued methods

--- model/server.go
+++ model/server.go
@@ -1,10 +1,5 @@
 package model
 
-import (
-	"fmt"
-	"time"
-)
-
 // ServerDefaults represents default settings for the server
 type ServerDefaults struct {
 	Bucket BucketSetting `json:"bucket"`
@@ -28,32 +23,6 @@ type LicenseInfo struct {
 	Fingerprint string `json:"fingerprint"`
 }
 
-// ParseLicenseInfo parses the original license info into a LicenseInfo struct
-func ParseLicenseInfo(data map[string]interface{}) (*LicenseInfo, error) {
-	expiryStr, ok := data["expiry_date"].(string)
-	if !ok {
-		return nil, NewAPIError("invalid expiry_date format", 400, nil)
-	}
-
-	expiryTime, err := time.Parse(time.RFC3339, expiryStr)
-	if err != nil {
-		return nil, NewAPIError("invalid expiry_date format", 400, err)
-	}
-
-	deviceNum, _ := data["device_number"].(float64)
-	diskQuota, _ := data["disk_quota"].(float64)
-
-	return &LicenseInfo{
-		Licensee:     data["licensee"].(string),
-		Invoice:      data["invoice"].(string),
-		ExpiryDate:   expiryTime.UnixMilli(),
-		Plan:         data["plan"].(string),
-		DeviceNumber: int64(deviceNum),
-		DiskQuota:    int64(diskQuota),
-		Fingerprint:  data["fingerprint"].(string),
-	}, nil
-}
-
 // ServerInfo represents information about the storage server
 type ServerInfo struct {
 	// Version storage server
@@ -72,60 +41,4 @@ type ServerInfo struct {
 	License *LicenseInfo `json:"license,omitempty"`
 	// Default settings
 	Defaults ServerDefaults `json:"defaults"`
-}
-
-// ParseServerInfo parses the original server info into a ServerInfo struct
-func ParseServerInfo(data map[string]interface{}) (*ServerInfo, error) {
-	bucketCount, _ := data["bucket_count"].(string)
-	uptime, _ := data["uptime"].(string)
-	usage, _ := data["usage"].(string)
-	oldestRecord, _ := data["oldest_record"].(string)
-	latestRecord, _ := data["latest_record"].(string)
-
-	var license *LicenseInfo
-	if licenseData, ok := data["license"].(map[string]interface{}); ok {
-		var err error
-		license, err = ParseLicenseInfo(licenseData)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	defaults := ServerDefaults{}
-	if defaultsData, ok := data["defaults"].(map[string]interface{}); ok {
-		if bucketData, ok := defaultsData["bucket"].(map[string]interface{}); ok {
-			quotaSize, _ := bucketData["quota_size"].(float64)
-			maxBlockSize, _ := bucketData["max_block_size"].(float64)
-			maxBlockRecords, _ := bucketData["max_block_records"].(float64)
-			quotaType, _ := bucketData["quota_type"].(string)
-
-			defaults.Bucket = BucketSetting{
-				QuotaSize:       int64(quotaSize),
-				MaxBlockSize:    int64(maxBlockSize),
-				MaxBlockRecords: int64(maxBlockRecords),
-				QuotaType:       QuotaType(quotaType),
-			}
-		}
-	}
-
-	return &ServerInfo{
-		Version:      data["version"].(string),
-		BucketCount:  parseStringToInt64(bucketCount),
-		Uptime:       parseStringToInt64(uptime),
-		Usage:        parseStringToInt64(usage),
-		OldestRecord: parseStringToInt64(oldestRecord),
-		LatestRecord: parseStringToInt64(latestRecord),
-		License:      license,
-		Defaults:     defaults,
-	}, nil
-}
-
-// Helper function to parse string to int64
-func parseStringToInt64(s string) int64 {
-	var result int64
-	_, err := fmt.Sscanf(s, "%d", &result)
-	if err != nil {
-		return 0
-	}
-	return result
 }
